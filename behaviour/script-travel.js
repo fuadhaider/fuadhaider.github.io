c46c1e9b0ea8b7335d1a3fbc4e6ccf461f5dfce3
fuadhaider <mohaiderfuad@gmail.com>
2018-01-07 23:59:40 +0000

    thumbnail gallery done

--- behaviour/script-travel.js
+++ behaviour/script-travel.js
@@ -1,8 +1,10 @@
 function parallax() {
-    var currentY = window.pageYOffset;
-    var travelImage1 = document.getElementById('travel-image-1');
-    var travelImage2 = document.getElementById('travel-image-2');
-    travelImage1.style.bottom = -(currentY)*0.25 + 'px';
-    travelImage2.style.bottom = -(currentY)*0.15 + 'px';
+  document.getElementById('scroll-icon').style.display = 'none';
+
+  var currentY = window.pageYOffset;
+  var travelImage1 = document.getElementById('travel-image-1');
+  var travelImage2 = document.getElementById('travel-image-2');
+  travelImage1.style.bottom = -(currentY)*0.25 + 'px';
+  travelImage2.style.bottom = -(currentY)*0.15 + 'px';
 }
 window.addEventListener('scroll', parallax, false);
